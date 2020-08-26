8b462c8e8d66be7705e0c0a4c6a009902da97abd
danho <danho@wp.pl>
2020-08-26 20:43:59 +0200

    bluetooth ciag dalszy

--- app/src/main/java/com/example/tank_controller/MainActivity.kt
+++ app/src/main/java/com/example/tank_controller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tank_controller
 
+import android.Manifest
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
@@ -7,32 +8,39 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.tank_controller.settings.SettingsActivity
 import org.jetbrains.anko.toast
+import java.io.IOException
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    var bluetoothReciver : BluetoothReciver? = null
+    //var bluetoothReciver : BluetoothReciver? = null
     var bluetoothService : BluetoothService = BluetoothService.instance
     val REQUEST_ENABLE_BLUETOOTH = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ConfigureReceiver()
+        //CheckPermissions()
+        //ConfigureReceiver()
         InitBluetooth()
         InitButtons()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(bluetoothReciver)
-    }
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        unregisterReceiver(bluetoothReciver)
+//    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -50,14 +58,36 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun ConfigureReceiver() {
-        val filter = IntentFilter()
-        filter.addAction(BluetoothDevice.ACTION_FOUND)
-        filter.addAction(BluetoothDevice.ACTION_PAIRING_REQUEST)
-        filter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED)
-        bluetoothReciver = BluetoothReciver()
-        registerReceiver(bluetoothReciver, filter)
+    private fun CheckPermissions() {
+        val permission1 = ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH)
+        val permission2 = ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADMIN)
+        val permission3 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+        val permission4 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        if (permission1 != PackageManager.PERMISSION_GRANTED ||
+            permission2 != PackageManager.PERMISSION_GRANTED ||
+            permission3 != PackageManager.PERMISSION_GRANTED
+            || permission4 != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION),
+                642)
+        } else {
+            Log.d("DISCOVERING-PERMISSIONS", "Permissions Granted")
+        }
     }
+
+//    private fun ConfigureReceiver() {
+//
+//        val filter = IntentFilter()
+//        filter.addAction(BluetoothDevice.ACTION_FOUND)
+//        filter.addAction(BluetoothDevice.ACTION_PAIRING_REQUEST)
+//        filter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED)
+//        //bluetoothReciver = BluetoothReciver()
+//        registerReceiver(bluetoothReciver, filter)
+//    }
 
 
 
@@ -73,16 +103,44 @@ class MainActivity : AppCompatActivity() {
             val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
         }
-
+/*
         if(bluetoothAdapter!!.isDiscovering()){
             bluetoothAdapter!!.cancelDiscovery()
         }
 
-        if(bluetoothAdapter!!.bondedDevices.any()) {
+ */
 
-        } else {
+        if(bluetoothAdapter!!.bondedDevices.any()) {
+//            val macAddress:String = "98:D3:31:FB:55:E6"
+            val macAddress:String = "D0:7F:A0:18:E3:D8"
+
+            val password = "1234"
+            val device =
+                bluetoothAdapter!!.bondedDevices.first{ it.address == macAddress }
+            val myUIID : UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                //device!!.setPin(password.toByteArray())
+                //device!!.setPairingConfirmation(true)
+
+            }
+            var  bluetoothSocket = device!!.createInsecureRfcommSocketToServiceRecord(myUIID)
+            BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
+
+            bluetoothSocket!!.connect()
+            val command:String = "A000"
+            try{
+                bluetoothSocket!!.outputStream.write(command.toByteArray())
+            } catch (e: IOException){
+                e.printStackTrace()
+            }
+        }
+        /*
+        else {
             bluetoothAdapter!!.startDiscovery()
         }
+
+         */
     }
 
     fun InitButtons() {
@@ -106,10 +164,16 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class BluetoothReciver: BroadcastReceiver() {
+/*class BluetoothReciver: BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == BluetoothDevice.ACTION_FOUND) {
+        print("BroadcastReceiver.OnReceive")
+        if (intent.action == BluetoothDevice.ACTION_FOUND || intent.action == BluetoothDevice.ACTION_PAIRING_REQUEST) {
             val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                device.createBond()
+            }
         }
     }
 }
+
+ */
